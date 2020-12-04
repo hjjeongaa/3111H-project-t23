89302c252ce5be1db905b889f2179d0fba4ced74
Ryder Khoi Daniel <ryderdaniel83@gmail.com>
2020-12-04 14:46:55 +0800

    remove controller and ui.fxml

--- comp3111-popnames-master/src/test/java/comp3111/popnames/controllers/TopNNamesControllerTest.java
+++ comp3111-popnames-master/src/test/java/comp3111/popnames/controllers/TopNNamesControllerTest.java
@@ -1,10 +1,22 @@
 package comp3111.popnames.controllers;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class TopNNamesControllerTest {
 
 	private Scene mainscene;
 	
+	// Error labels
+	private Label invalidStart;
+	private Label invalidEnd;
+	private Label invalidStartRange;
+	private Label invalidEndRange;
+	private Label invalidNError1;
+	private Label invalidNError2;
+	
+	// User Input components
+	private TextField startYear;
 	
 }
